19811a690e75eab38a15c2730c1bf516e5c3a52d
eliphas <eliphas.sn@gmail.com>
2022-10-01 18:40:32 +0300

    Site create

---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+@extends('layout.app')
+@section('content')
+    <section class="header">
+        <div class="inf">
+            <ul>
+                <li>advocat.bogachalla@gmail.com</li>
+                <li>Украина, Запорожье</li>
+            </ul>
+            <p>Социальные сети</p>
+        </div>
+        <div class="banner">
+            <div class="nav">
+                <div class="logo"><img src="" alt=""></div>
+                <ul>
+                    <li>Список</li>
+                    <li>Список</li>
+                    <li>Список</li>
+                    <li>Список</li>
+                </ul>
+            </div>
+            <div class="text">
+                <em>Корпоративный адвокат</em>
+                <h2>Адвокат Алла Богач</h2>
+                <a href="#">Узнать больше</a>
+            </div>
+        </div>
+    </section>
+@stop
